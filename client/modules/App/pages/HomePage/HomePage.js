2c4d6e0c670d80d31c55910e195874572029ee5e
Schmid <schmid@maghire.com>
2020-01-13 21:43:42 +0800

    Initial Setup

---
+++ client/modules/App/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to Pop-Charge.</h2>
+        <h6>It's mobile app page.</h6>
+      </div>
+    )
+  }
+}
+
+export default HomePage;
